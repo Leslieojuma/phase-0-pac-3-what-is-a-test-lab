637c5a06d642881bafc821f23b2f7a9efdd5fb53
leslie <leslie.ojuma@student.moringaschool.com>
2025-05-31 08:56:23 +0300

    pac3

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+module.exports.name = "Susan";
+module.exports.height = 35;
+module.exports.message = `${module.exports.name} is ${module.exports.height} inches tall`;
